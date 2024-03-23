bb9e6b74f31d41a066188008463c105a500e1ee3
Guilherme <guilherme.novais.leite@gmail.com>
2024-03-23 19:08:12 -0300

    feat: add home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-gradient-to-b from-slate-50 to-green-50 h-screen`}
+        className={`${inter.className} bg-gradient-to-b from-slate-50 to-green-50`}
       >
         <Navbar />
         {children}
