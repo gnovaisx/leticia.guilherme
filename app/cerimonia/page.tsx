48edec5bf9049c4037d419efe349bcb1ede2835b
Guilherme Novais <guilherme.novais@cvh.com.br>
2024-05-06 07:52:07 -0300

    resposive map

--- app/cerimonia/page.tsx
+++ app/cerimonia/page.tsx
@@ -37,7 +37,7 @@ export default function Cerimonia() {
           </p>
           <p>Aguardamos ansiosamente a presença de todos.</p>
         </div>
-        <div className="flex self-center justify-end">
+        <div className="flex self-center justify-center lg:justify-end">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3682.1995632517715!2d-47.199552523746156!3d-22.646346879439193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c893445fbaac77%3A0x72c05435323f738c!2sNonno%20Vit%C3%B3rio%20Trattoria%20%7C%20Restaurante!5e0!3m2!1spt-BR!2sbr!4v1714752564236!5m2!1spt-BR!2sbr"
             style={{ border: 0, width: "600px", height: "450px" }}
